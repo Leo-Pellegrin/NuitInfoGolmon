b79c3aa9f2af2cd7916473cb550062799bbc4fee
Leo-Pellegrin <leo.pellegrin@etud.univ-jfc.fr>
2023-12-08 04:15:16 +0100

    Test

--- src/layouts/default/MainView.vue
+++ src/layouts/default/MainView.vue
@@ -7,3 +7,12 @@
 <script setup>
   //
 </script>
+
+<style>
+ .v-app{
+    background-image: url("../../assets/background_light.png");
+    background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
+  }
+</style>
--- src/views/PageAccueil.vue
+++ src/views/PageAccueil.vue
@@ -8,3 +8,11 @@
   import MainPage from '@/components/MainPage.vue'
 </script>
 
+<style>
+body{
+    background-image: url("../assets/background_light.png");
+    background-size: cover; /* Ensures the image covers the entire background */
+    background-position: center center; /* Center the image */
+    background-repeat: no-repeat; /* Do not repeat the image */
+}
+</style>
